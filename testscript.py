4657a69ce146be4ae3aa950bd3b529a6ebc9ae9e
Tomasz K <tomasz.klos71@gmail.com>
2018-10-16 08:23:49 +0200

    s

--- testscript.py
+++ testscript.py
@@ -1,16 +1,41 @@
+import math
+from time import sleep
+
+def printLogarithm(x):
+    if x <= 0:
+        print "Positive numbers only, please."
+        return
+    result = math.log(x)
+    print "The log of x is", result
+
+def countdown(n):
+    if n == 0:
+        print("Blastoff!")
+    else:
+        print(n)
+        sleep(1)
+        countdown(n-1)
+
+def area(radius):
+    temp = math.pi * radius**2
+    return temp
+
+print(area(input('give the circle radius: ')))
 
 
 def newLine():
     print('')
 
-print('linia 1')
-newLine()
-print('linia 2')
+def getShitDone():
+    task = input('Give me your task difficulty from 1 to 5: ')
+    time = input('How much time do you have for it in hours: ')
+    effort = task*time
+    if effort > 20:
+        print('Dude, you are fucked!')
+    else:
+        print('Come one, you can do that!')
 
-a = input('podaj a: ')
-b = input('podaj b: ')
 
-if a<b:
-    print('a is less than b')
-else:
-    print('test na B')
+def justNewFunction(tomasz, weather):
+    alexis = tomasz*weather+weather
+    print(alexis);
